56c0505ed2f9d05fdacc56dd02428b0ce648ba05
thetrucy <nguyenthanhtruc.9a4.28@gmail.com>
2026-03-25 16:49:41 +0700

    update heuristic function

--- src/freecell/solvers/Astar.py
+++ src/freecell/solvers/Astar.py
@@ -15,7 +15,6 @@ class AstarSolver(BestFSSolver):
         g: number of moves taken from start.
         h: estimated moves to goal.
         """
-        gamestate = state.to_game_state()
         # 1. Calculate the 'Step Cost' (Weight)
         weight = 1
         if move:
@@ -27,10 +26,8 @@ class AstarSolver(BestFSSolver):
                 weight = 3 # Discouraged (resource consumption)
         new_g = parent_g + weight
         # 1. Higher foundation count reduces cost
-        remain_cards = gamestate.cards_remaining
+        remain_cards = state.cards_remaining()
         # 2. Empty freecells are good for mobility
-        empty_freecells = gamestate.empty_freecell_count()
-        # 3. Blocked cards (cards in cascades that are not in order)
-        misplaced_cards = gamestate.disorder_count()
-        h = remain_cards + misplaced_cards - empty_freecells
+        empty_freecells = state.freecell_count_empty()
+        h = remain_cards - empty_freecells
         return (new_g + h * 2, weight)
