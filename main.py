c4c514fe195ca0180d8ec0dac92720cf94b5f621
Minh Thu <9a1.24.lephamminhthu@gmail.com>
2026-03-26 09:16:25 +0700

    add src directory to sys.path for freecell import

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+# Add the 'src' directory to sys.path so 'freecell' can be imported
+src_path = Path(__file__).parent / "src"
+sys.path.insert(0, str(src_path.resolve()))
+
 from freecell.GUI.app import run
 
 
